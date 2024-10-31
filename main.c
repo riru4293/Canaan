9d5549c6a4af58eb0ea8d3a715a3f74eeabc5438
riru4293 <41512570+riru4293@users.noreply.github.com>
2024-10-31 22:44:17 +0900

    USB-CDC trial

--- main.c
+++ main.c
@@ -1,17 +1,29 @@
 /* -------------------------------------------------------------------------- */
 /* Include                                                                    */
 /* -------------------------------------------------------------------------- */
-#include <stdlib.h>
 #include <pico/stdlib.h>
+#include <bsp/board_api.h>
+#include <tusb.h>
 #include <FreeRTOS.h>
 #include <task.h>
+#include <semphr.h>
+#include <queue.h>
+#include <timers.h>
 
 /* -------------------------------------------------------------------------- */
 /* Macro                                                                      */
 /* -------------------------------------------------------------------------- */
 #define LED_PORT (25U)
+
+#define HEARTBEAT_PRIORITY (1U)
+#define HEARTBEAT_STACK_SIZE (configMINIMAL_STACK_SIZE)
 #define HEARTBEAT_INTERVAL_MS (250U)
-#define TASK_HEARTBEAT_PRIORITY (1U)
+
+#define USBD_PRIORITY (3U)
+#define USBD_STACK_SIZE (3 * configMINIMAL_STACK_SIZE / 2) * (CFG_TUSB_DEBUG ? 2 : 1)
+
+#define CDC_PRIORITY (2U)
+#define CDC_STACK_SIZE (configMINIMAL_STACK_SIZE)
 
 /* -------------------------------------------------------------------------- */
 /* Type definition                                                            */
@@ -21,38 +33,53 @@
 /* Prototype                                                                  */
 /* -------------------------------------------------------------------------- */
 static void heartbeatTask(void *nouse);
+static void usbdTask(void *nouse);
+static void cdcTask(void *nouse);
 
 /* -------------------------------------------------------------------------- */
 /* Global                                                                     */
 /* -------------------------------------------------------------------------- */
-static TaskHandle_t gHeartbeatTask = NULL;
+static TaskHandle_t gHbTaskHndl = NULL;
+static TaskHandle_t gUsbdTaskHndl = NULL;
+static TaskHandle_t gCdcTaskHndl = NULL;
+
+static StaticTask_t gHbTaskDef;
+static StaticTask_t gUsbdTaskDef;
+static StaticTask_t gCdcTaskDef;
+
+static StackType_t gHbStack[HEARTBEAT_STACK_SIZE];
+static StackType_t gUsbdStack[USBD_STACK_SIZE];
+static StackType_t gCdcStack[CDC_STACK_SIZE];
 
 /* -------------------------------------------------------------------------- */
 /* Public function                                                            */
 /* -------------------------------------------------------------------------- */
 int main(void)
 {
-    BaseType_t result;
+    /* Initialize board. */
+    board_init();
 
-    /* Initialize LED */
+    /* Initialize LED. */
     gpio_init(LED_PORT);
     gpio_set_dir(LED_PORT, GPIO_OUT);
 
-    /* Creates a tasks */
-    result = xTaskCreate(heartbeatTask, "Heartbeat", configMINIMAL_STACK_SIZE,
-                         NULL, TASK_HEARTBEAT_PRIORITY, &gHeartbeatTask);
+    /* Creates a tasks. */
+    gHbTaskHndl = xTaskCreateStatic(heartbeatTask, "hb", HEARTBEAT_STACK_SIZE,
+                                    NULL, HEARTBEAT_PRIORITY, gHbStack, &gHbTaskDef);
 
-    if (pdPASS == result)
+    gUsbdTaskHndl = xTaskCreateStatic(usbdTask, "usbd", USBD_STACK_SIZE,
+                                      NULL, USBD_PRIORITY, gUsbdStack, &gUsbdTaskDef);
+
+    gCdcTaskHndl = xTaskCreateStatic(cdcTask, "cdc", CDC_STACK_SIZE,
+                                     NULL, CDC_PRIORITY, gCdcStack, &gCdcTaskDef);
+
+    /* Start task scheduking. */
+    vTaskStartScheduler();
+
+    while (true)
     {
-        vTaskStartScheduler();
-
-        while (true)
-        {
-            /* Unreachable */
-        }
+        /* Unreachable */
     }
-
-    exit(EXIT_FAILURE);
 }
 
 /* -------------------------------------------------------------------------- */
@@ -72,5 +99,57 @@ static void heartbeatTask(void *nouse)
 
         /* Delay 500ms */
         vTaskDelay(HEARTBEAT_INTERVAL_MS);
+    }
+}
+
+static void usbdTask(void *nouse)
+{
+    /* Init device stack on configured roothub port.                                        */
+    /* This should be called after scheduler/kernel is started.                             */
+    /* Otherwise it could cause kernel issue since USB IRQ handler does use RTOS queue API. */
+    tud_init(BOARD_TUD_RHPORT);
+    board_init_after_tusb();
+
+    while (true)
+    {
+        /* Put this thread to waiting state until there is new events. */
+        tud_task();
+
+        /* Following code only run if tud_task() process at least 1 event. */
+        tud_cdc_write_flush();
+
+        /* CFG_TUSB_OS=OPT_OS_FREERTOSに設定できれば、tud_taskがFreeRTOSの   */
+        /* タスク制御を行うが、pico-sdk(2.0.0)のissueにより設定できない。    */
+        /* そのため無限ループ化するので、1msループとなるようにしている。     */
+        /* Issue解消後は、本1ms待機を削除すること。                          */
+        vTaskDelay(1);
+    }
+}
+
+static void cdcTask(void *nouse)
+{
+    while (true)
+    {
+        /* Connected check for DTR bit.                                      */
+        /* Most but not all terminal client set this when making connection. */
+        if (tud_cdc_connected())
+        {
+            /* There are data available. */
+            while (tud_cdc_available())
+            {
+                uint8_t buff[64];
+
+                /* Read a characters. */
+                uint32_t count = tud_cdc_read(buff, sizeof(buff));
+                (void)count;
+
+                /* Echo back. */
+                tud_cdc_write(buff, count);
+            }
+
+            tud_cdc_write_flush();
+        }
+
+        vTaskDelay(1);
     }
 }
